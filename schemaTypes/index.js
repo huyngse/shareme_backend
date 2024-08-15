60123b124ff0a62443c8e8afa9ab1eaaadcbe31f
huyngse <ngogiahuy775309@gmail.com>
2024-08-15 08:48:47 +0700

    Add schemas

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,2 +1,6 @@
 import user from './user'
-export const schemaTypes = [user]
+import pin from './pin'
+import postedBy from './postedBy'
+import save from './save'
+import comment from './comment'
+export const schemaTypes = [user, pin, postedBy, save, comment]
